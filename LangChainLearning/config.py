1ce6ed2520767fc74b0334f67a5cc604e4cefbdd
Gerson Dalto <gersondalto@gmail.com>
2026-04-11 10:01:47 -0300

    ajustado para mac

--- LangChainLearning/config.py
+++ LangChainLearning/config.py
@@ -1,13 +1,10 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 os.environ["OPENAI_API_KEY"] = os.getenv("MYOPENAI_API_KEY", "")
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("MYOPENAI_API_KEY")
 
 if not OPENAI_API_KEY:
-    raise ValueError("OPENAI_API_KEY não encontrada no ambiente.")
+    raise ValueError("OPENAI_API_KEY não encontrada")
 
-
+print(OPENAI_API_KEY)
